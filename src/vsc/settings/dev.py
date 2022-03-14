bb68d0e3f9d1e8c5b2c5e60285d9010a5c7a50c7
hamed <ahmadihamed167@gmail.com>
2022-03-14 20:23:29 +0330

    EMAIL settings added

--- src/vsc/settings/dev.py
+++ src/vsc/settings/dev.py
@@ -1,6 +1,7 @@
 from .base import * 
 from core.utils import get_secret
 
+
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -24,3 +25,13 @@ DATABASES = {
 
 USE_TZ =False 
 TIME_ZONE = 'Asia/Tehran'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+"""
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = get_secret("EMAIL_USERNAME")
+EMAIL_HOST_PASSWORD = get_secret("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+"""
