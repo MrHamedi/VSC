3b99010774a753926fc2ab2aaf9d7e585678d8ab
hamed <ahmadihamed167@gmail.com>
2022-04-17 20:25:53 +0430

    if debug added

--- src/vsc/settings/dev.py
+++ src/vsc/settings/dev.py
@@ -26,12 +26,12 @@ DATABASES = {
 USE_TZ =False 
 TIME_ZONE = 'Asia/Tehran'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-"""
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = get_secret("EMAIL_USERNAME")
-EMAIL_HOST_PASSWORD = get_secret("EMAIL_PASSWORD")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-"""
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = get_secret("EMAIL_USERNAME")
+    EMAIL_HOST_PASSWORD = get_secret("EMAIL_PASSWORD")
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
