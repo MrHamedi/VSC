bcb5b5953d0800b734e1eaf106d6dcee316eabf8
hamed <ahmadihamed167@gmail.com>
2022-04-19 22:51:17 +0430

    Profile model updated

--- src/accounts/models/profile.py
+++ src/accounts/models/profile.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 from django.conf import settings 
 from django.urls import reverse 
@@ -5,11 +6,11 @@ from django.urls import reverse
 
 class Profile(models.Model):
     image=models.ImageField(upload_to="profiles/%Y/%m/%d",null=True,blank=True)
-    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    user=models.OneToOneField(settings.AUTH_USER_MODEL ,on_delete=models.CASCADE ,related_name='profile')
 
-#    def get_absolute_url(self):
-#        return reverse("")
-
+    class Meta:
+        verbose_name="پروفایل"
+        verbose_name_plural="پروفایل ها"
 
     def __str__(self):
-        return(self.user)
+        return(self.user.username)
