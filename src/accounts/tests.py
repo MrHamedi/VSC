ff3886ff90f9b4064cd7b4f8107e22eab7cfa5fb
hamed <ahmadihamed167@gmail.com>
2022-04-19 22:52:06 +0430

    Profile tests added

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Profile
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class ProfileTest(TestCase):
+
+    def setUp(self):
+        user=User.objects.create(username='username',password='password')
+
+
+    def test_profile_fields(self):
+        profile=Profile.objects.get(id=1)
+        self.assertEqual(profile.user.username,"username")
+
+
