9601f548f339e57b05093750f9c35ed02fc3c3cf
hamed <ahmadihamed167@gmail.com>
2022-05-12 21:03:12 +0430

    ~/vsc/vsc/requirements/base.txt

--- src/like/models/like.py
+++ src/like/models/like.py
@@ -6,8 +6,8 @@ from comment.models import Comment
 
 # Create your models here.
 class Like(models.Model):
-    
-    comment=models.ForeignKey(Comment,on_delete=models.CASCADE,related_name="comments_likes")
+
+    comment=models.ForeignKey(Comment,on_delete=models.CASCADE,related_name="comments_likes")    
     video=models.ForeignKey(Video,on_delete=models.CASCADE,related_name="videos_likes")
     user=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name="user_likes")
 
