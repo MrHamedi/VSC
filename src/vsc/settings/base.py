78bdd4513c7a8eae818b3d95eb9a0a77010eed45
hamed <ahmadihamed167@gmail.com>
2022-05-13 18:31:10 +0430

    debug_toolbar will be added to MIDDLEWARES from dev settings instead of base

--- src/vsc/settings/base.py
+++ src/vsc/settings/base.py
@@ -59,13 +59,14 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
+
+
 
 ROOT_URLCONF = 'vsc.urls'
 
