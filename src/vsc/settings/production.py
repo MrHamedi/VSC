9328435b0204375581ae1fc607e6fab2d21985de
darklord <ahmadihamed167@gmail.com>
2023-05-23 17:00:21 +0330

    setting changed for pythonanywhere

--- src/vsc/settings/production.py
+++ src/vsc/settings/production.py
@@ -6,8 +6,8 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "vsc_db",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secret("DB_NAME"),
         'USER': get_secret("DB_USER"),
         "PASSWORD": get_secret("DB_PASSWORD"),
         'HOST': '127.0.0.1',
