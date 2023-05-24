b5cc023b50088132b913ae23c498740a0d2cc136
darklord <ahmadihamed167@gmail.com>
2023-05-24 17:20:04 +0330

    mysql does not allow unique fields be longer than 300 chars

--- src/video/models/Video.py
+++ src/video/models/Video.py
@@ -2,7 +2,6 @@ from django.db import models
 from accounts.models.profile import Profile
 from core.models import CommonPostableFields
 from core.utils import upload_to_username ,upload_image_to_username
-from ..utils.set_image import set_image
 from ..validators import video_validator
 from django.urls import reverse
 import os
@@ -12,7 +11,7 @@ from taggit.managers import TaggableManager
 class Video(CommonPostableFields):
 
     video=models.FileField(upload_to=upload_to_username ,validators=[video_validator])
-    slug=models.SlugField(unique=True ,max_length=300,blank=True ,null=True)
+    slug=models.SlugField(unique=True ,max_length=255,blank=True ,null=True)
     subject=models.CharField(max_length=150)
     description=models.TextField(null=True,blank=True)
     image=models.ImageField(upload_to=upload_image_to_username ,null=True ,blank=True)
@@ -35,6 +34,3 @@ class Video(CommonPostableFields):
     def extension_splitter(self):
         return os.path.splitext(self.video.path)[1]
 
-    def save(self,*args,**kwargs):
-        super(Video,self).save(*args,**kwargs)
-        set_image(self)
